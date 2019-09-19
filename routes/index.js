65cdb784c766815fcdb5b2f78dbc24b5018f42ab
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2019-09-20 08:27:12 +0900

    @===^ㅇ^==@

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/web', function(req, res, next){
+  res.render('../views/category/web/web.ejs');
+});
+
 module.exports = router;
