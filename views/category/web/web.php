65a27891be31f24f0a0ac1c9e692a902bdc42a12
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2019-09-20 09:39:42 +0900

    web.ejs -> web.php

--- views/category/web/web.php
+++ views/category/web/web.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel='stylesheet' href='/stylesheets/webcategory.css'>
-    <title>IVIAS :: 웹</title>
+    <title>IVIAS :: 웹 카테고리</title>
 </head>
 <body>
     <div class="m_sc">
